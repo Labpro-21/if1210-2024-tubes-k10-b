2db643f85a070e8e6e60ed7c654fcd482f64a716
Ellipsis <19623149@std.stei.itb.ac.id>
2024-04-28 14:25:06 +0700

    fix: changed seed to be automatic

--- src/RandomNumberGenerator.py
+++ src/RandomNumberGenerator.py
@@ -1,8 +1,8 @@
-def random_number_generator(a: int, b: int, seed: int=42) -> int:
+import time
+
+def random_number_generator(a: int, b: int, seed: int=int(time.time())) -> int:
     m: int = 2**31 - 1 
-
     seed: int = (a * seed + b) % m
-
     random_number: int = a + seed % (b - a + 1)
 
     return random_number

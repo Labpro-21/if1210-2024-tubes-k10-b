750241ea2877c92818e81a638d4e9d309af08e8f
Ellipsis <19623149@std.stei.itb.ac.id>
2024-05-06 16:27:08 +0700

    feat: added Inventory.py for Agent

---
+++ src/Inventory.py
@@ -0,0 +1,97 @@
+from .helper.ListManipulation import to_list
+from .helper.Readline import readlines
+from .helper.IsType import is_number
+from typing import List, Tuple
+
+
+def get_item_data(idn: str) -> List[List[str]]:
+    item: List[List[str]] = to_list(readlines('./data/item_inventory.csv'))
+    arr = []
+    for i in item:
+        if i[0] == idn:
+            arr.append(i)
+    return arr
+
+
+def get_monster_data(idn: str) -> List[List[str]]:
+    monster: List[List[str]] = to_list(
+        readlines('./data/monster_inventory.csv'))
+    arr = []
+    for i in monster:
+        if i[0] == idn:
+            arr.append(i)
+    return arr
+
+
+def concat_monster(a1: List[List[str]], a2: List[List[str]]) -> None:
+    for i in range(len(a1)):
+        for j in a2:
+            if a1[i][1] == j[0]:
+                for k in range(1, 5):
+                    a1[i].append(j[k])
+
+
+def concat_monster_item(a1: List[List[str]], a2: List[List[str]]) -> List[Tuple[str, List[str]]]:
+    arr = []
+    for i in a1:
+        arr.append(('monster', i))
+    for i in a2:
+        arr.append(('potion', i))
+    return arr
+
+
+'''
+LOGIN
+b
+b
+INVENTORY
+'''
+
+
+def inventory(data: List[str]) -> None:
+    print("============ INVENTORY LIST (User ID: %s) ============" % (data[0]))
+    print("Jumlah O.W.C.A. Coin-mu sekarang %s." % (data[4]))
+    monster_inv: List[List[str]] = get_monster_data(data[0])
+    monster: List[List[str]] = to_list(readlines('./data/monster.csv'))
+    concat_monster(monster_inv, monster)
+    item_inv: List[List[str]] = get_item_data(data[0])
+    num: int = 1
+    all_data: List[List[str]] = concat_monster_item(monster_inv, item_inv)
+    for inv in monster_inv:
+        print("%s. Monster\t(Name: %s, Lvl: %s, HP: %s)" %
+              (num, inv[3], inv[2], inv[6]))
+        num += 1
+
+    kamus_potion = {
+        "strength": "ATK",
+        "healing": "Heal",
+        "resilience": "DEF"
+    }
+    for inv in item_inv:
+        print("%s. Potion\t(Type: %s, Qty: %s)" %
+              (num, kamus_potion[inv[1]], inv[2]))
+        num += 1
+
+    c = ""
+    while c != "KELUAR":
+        print("\nKetikkan id untuk menampilkan detail item:")
+        c = input(">>> ")
+        if c != "KELUAR" and is_number(c):
+            if 1 <= int(c) <= len(all_data):
+                c = int(c)-1
+                if all_data[c][0] == 'monster':
+                    print("Monster")
+                    print("Name\t  : %s" % (all_data[c][1][3]))
+                    print("ATK Power : %s" % (all_data[c][1][4]))
+                    print("DEF Power : %s" % (all_data[c][1][5]))
+                    print("HP\t  : %s" % (all_data[c][1][6]))
+                    print("Level\t  : %s" % (all_data[c][1][2]))
+                elif all_data[c][0] == 'potion':
+                    print("Potion")
+                    print("Type      : %s" % (kamus_potion[all_data[c][1][1]]))
+                    print("Quantity  : %s" % (all_data[c][1][2]))
+            else:
+                print("Indeks yang dimasukkan tidak ada!")
+        
+        elif c != 'KELUAR':
+            print("Masukkan bilangan integer positif!")

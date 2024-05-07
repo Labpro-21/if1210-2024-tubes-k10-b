8d11745f7e84a218e132b3edebf739519178e171
Ellipsis <19623149@std.stei.itb.ac.id>
2024-05-08 00:14:15 +0700

    feat: added help messae for 3 categories

---
+++ src/Help.py
@@ -0,0 +1,22 @@
+def help_message_agent():
+    print("Menu yang tersedia:")
+    print("[HELP]\t\tMembantu untuk melihat command yang ada")
+    print("[INVENTORY]\tUser melihat penyimpanan")
+    print("[SHOP]\t\tUser Agent membeli monster dan potion")
+    print("[LABORATORY]\tAgent melakukan upgrade monster")
+    print("[ARENA]\t\tAgent melatih monster")
+    print("[BATTLE]\tAgent melakukan battle")
+    print("[LOGOUT]\tAgent melakukan logout akun")
+    print()
+
+def help_message_admin():
+    print("Menu yang tersedia:")
+    print("[HELP]\t\tMembantu untuk melihat command yang ada")
+    print("[SHOP]\t\tAdmin mengatur manajemen Shop and Currency")
+    print("[MONSTER]\tAdmin mengatur manajemen Monster")
+
+def help_message_before_login():
+    print("Menu yang tersedia:")
+    print("[HELP]\t\tMembantu untuk melihat command yang ada")
+    print("[REGISTER]\tUser melakukan registrasi akun")
+    print("[LOGIN]\t\tUser melakukan login akun")
